1be0d580cffbe2cce8d41807908a806d4166405a
Nerz <adrian.dufac@gmail.com>
2024-10-11 11:40:35 +0200

    extracting error message to reuse

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,14 @@
+import { Text } from "@radix-ui/themes";
+import React, { PropsWithChildren } from "react";
+
+const ErrorMessage = ({ children }: PropsWithChildren) => {
+  // PropsWithChildren is a type that allows us to use children prop (no need to define an interface )
+  if (!children) return null;
+  return (
+    <Text color="red" as="p">
+      {children}
+    </Text>
+  );
+};
+
+export default ErrorMessage;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod"; // zodResolver is a hook that allows us to use Zod schemas with react-hook-form and have client validation
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type NewIssueForm = z.infer<typeof createIssueSchema>; // creating type directly from Zod schema
 
@@ -45,11 +46,7 @@ const NewIssuePage = () => {
           placeholder="Title"
           {...register("title")}
         ></TextField.Root>
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller // SimpleMDE doesn't support ref, so we use Controller
           name="description"
           control={control}
@@ -57,11 +54,8 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+
         <Button>Submit New Issue</Button>
       </form>
     </div>
