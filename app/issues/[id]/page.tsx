29a2aa73c0a701ee3a51aa331fb8181d44fdaa38
Nerz <adrian.dufac@gmail.com>
2024-10-14 12:06:30 +0200

    addition of edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import ReactMarkdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface IssueDetailPageProps {
   params: { id: string };
@@ -24,17 +26,26 @@ const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
   }
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="3" my="2">
-        <IssueStatusBadge status={issue.status}></IssueStatusBadge>
-        <Text>{issue.createdAt.toLocaleDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-        {/*by default tailwind doiesnt style lists so we installed tailwindcss/typography plugin  and the class prose is to tell that this container need to style lists*/}
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      {/*initial is for mobile and md is for medium devices*/}
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="3" my="2">
+          <IssueStatusBadge status={issue.status}></IssueStatusBadge>
+          <Text>{issue.createdAt.toLocaleDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+          {/*by default tailwind doiesnt style lists so we installed tailwindcss/typography plugin  and the class prose is to tell that this container need to style lists*/}
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Eddit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
