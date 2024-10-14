5bc8274057ad1de3a7e00a3f799235e4ca7e69a4
Nerz <adrian.dufac@gmail.com>
2024-10-14 11:54:32 +0200

    organizing imports

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,5 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import { Heading, Flex, Card, Box } from "@radix-ui/themes";
-import React from "react";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
+import { Box, Card, Flex } from "@radix-ui/themes";
 
 const LoadingIssuePage = () => {
   return (
