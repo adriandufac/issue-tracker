2ebda1d5517314803f4fc98d32d1583b4ef8719c
Nerz <adrian.dufac@gmail.com>
2024-10-18 09:11:04 +0200

    adding spinner and desable delete button while backend is deleting

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Spinner } from "@/app/components";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -7,21 +8,26 @@ import { useState } from "react";
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setUseDeleting] = useState(false);
   const handleDelete = async () => {
     try {
+      setUseDeleting(true);
       await axios.delete(`/api/issues/${issueId}`);
       router.push("/issues");
       router.refresh(); // to avoid caching and see our issue deleted
     } catch (error) {
+      setUseDeleting(false);
       setError(true);
-      console.error("An error occurred while deleting the issue");
     }
   };
   return (
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
+          <Button color="red" disabled={isDeleting}>
+            Delete Issue
+            {isDeleting && <Spinner></Spinner>}
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content maxWidth="450px">
           <AlertDialog.Title>Delete Issue</AlertDialog.Title>
