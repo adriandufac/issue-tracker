33c8ae66d017f320421c1e8111f158cedfdbc8ce
Nerz <adrian.dufac@gmail.com>
2024-10-29 14:35:51 +0100

    implementing pagination

--- app/issues/_components/Pagination.tsx
+++ app/issues/_components/Pagination.tsx
@@ -6,16 +6,25 @@ import {
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
-import React, { useState } from "react";
-import { set } from "zod";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
 
 interface PaginationProps {
   itemCount: number;
   pageSize: number;
   currentPage: number;
 }
+
 const Pagination = ({ itemCount, pageSize, currentPage }: PaginationProps) => {
-  const [page, setPage] = useState(currentPage);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
   const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount <= 1) return null;
   return (
@@ -23,20 +32,35 @@ const Pagination = ({ itemCount, pageSize, currentPage }: PaginationProps) => {
       <Text size="2">
         Page {currentPage} of {pageCount}
       </Text>
-      <Button color="gray" variant="soft" disabled={currentPage == 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage == 1}
+        onClick={() => changePage(1)}
+      >
         <DoubleArrowLeftIcon></DoubleArrowLeftIcon>
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage == 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage == 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <ChevronLeftIcon></ChevronLeftIcon>
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage == pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage == pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <ChevronRightIcon></ChevronRightIcon>
       </Button>
       <Button
         color="gray"
         variant="soft"
         disabled={currentPage == pageCount}
-        onClick={() => (currentPage += 1)}
+        onClick={() => changePage(pageCount)}
       >
         <DoubleArrowRightIcon></DoubleArrowRightIcon>
       </Button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import Pagination from "./issues/_components/Pagination";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <div>
-      <Pagination itemCount={100} pageSize={20} currentPage={5} />
+      <Pagination
+        itemCount={100}
+        pageSize={20}
+        currentPage={parseInt(searchParams.page)}
+      />
     </div>
   );
 }
