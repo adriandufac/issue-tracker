320fac068f8999375e5adf4379784885ff313999
Nerz <adrian.dufac@gmail.com>
2024-10-29 11:43:41 +0100

    imùplementing logic to sort issues

--- app/issues/(list)/page.tsx
+++ app/issues/(list)/page.tsx
@@ -11,24 +11,30 @@ const IssuePage = async ({
 }: {
   searchParams: { status: string; orderBy: keyof Issue };
 }) => {
-  const statuses = Object.values(Status).toString();
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
-  const issues = await prisma.issue.findMany({
-    where: {
-      status: status as Status,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-
   const columns: { label: string; value: keyof Issue; className?: string }[] = [
     { label: "Issue", value: "title" },
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
+
+  const statuses = Object.values(Status).toString();
+
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status: status as Status,
+    },
+    orderBy: orderBy,
+  });
 
   return (
     <div>
